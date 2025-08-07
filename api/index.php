5b47d82bbf862ac407bcaf5cdfd18ff96bd83e41
Andrés Álvarez Cid <andrew.cei@ciencias.unam.mx>
2025-08-06 21:45:13 -0600

    APPWith API-Key

---
+++ api/index.php
@@ -0,0 +1,56 @@
+<?php
+declare(strict_types=1);
+//require dirname(__DIR__) . "/api/src/TaskController.php";
+require __DIR__ . '/bootstrap.php';
+
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    header("Access-Control-Allow-Origin: http://localhost:5173");
+    header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+    header("Access-Control-Allow-Headers: Content-Type, Authorization, x-api-key");
+    http_response_code(200);
+    exit();
+}
+
+header("Access-Control-Allow-Origin: http://localhost:5173");
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, PATCH, DELETE");
+header("Access-Control-Allow-Headers *");
+
+
+$path = parse_url($_SERVER["REQUEST_URI"], PHP_URL_PATH);
+
+$parts = explode("/", $path);
+
+$resource = $parts[2];
+
+$id = $parts[3] ?? null;
+
+if ($resource != "tasks") {
+    //header("{$_SERVER['SERVER_PROTOCOL']} 404 Not Found");
+    http_response_code(404);
+    exit;
+}
+
+
+
+$database = new Database(
+    $_ENV["DB_HOST"],
+    $_ENV["DB_NAME"],
+    $_ENV["DB_USER"],
+    $_ENV["DB_PASS"]
+);
+
+$user_gateway = new UserGateway($database);
+
+$auth = new Auth($user_gateway);
+
+if (! $auth->authenticateAPIKey()) {
+    exit;
+}
+
+$user_id = $auth->getUserID();
+
+$task_gateway = new TaskGateway($database);
+
+$controller = new TaskController($task_gateway, $user_id);
+
+$controller->processRequest($_SERVER['REQUEST_METHOD'], $id);
---
+++ register.php
@@ -0,0 +1,61 @@
+<?php
+
+require __DIR__ . "/vendor/autoload.php";
+
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
+    $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+    $dotenv->load();
+
+    $database = new Database($_ENV["DB_HOST"], $_ENV["DB_NAME"], $_ENV["DB_USER"], $_ENV["DB_PASS"]);
+    $conn = $database->getConnection();
+
+    $sql = "INSERT INTO user (name, username, password_hash, api_key)
+    VALUES (:name, :username, :password_hash, :api_key)";
+
+    $stmt = $conn->prepare($sql);
+
+    $password_hash = password_hash($_POST["password"], PASSWORD_DEFAULT);
+    $api_key = bin2hex(random_bytes(16));
+
+    $stmt->bindValue(":name", $_POST["name"], PDO::PARAM_STR);
+    $stmt->bindValue(":username", $_POST["username"], PDO::PARAM_STR);
+    $stmt->bindValue(":password_hash", $password_hash, PDO::PARAM_STR);
+    $stmt->bindValue(":api_key", $api_key, PDO::PARAM_STR);
+    $stmt->execute();
+
+    echo "Gracias por registrarte. Tu API key es: ", $api_key;
+    exit;
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Register</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@picocss/pico@2/css/pico.min.css">
+</head>
+<body>
+    <main class="container">
+        <h1>Registro</h1>
+
+        <form action="" method="POST">
+            <label for="name">
+                Nombre:
+                <input type="text" name="name" id="name">
+            </label>
+            <label for="username">
+                Usuario:
+                <input type="text" name="username" id="username">
+            </label>
+            <label for="password">
+                Contraseña:
+                <input type="password" name="password" id="password">
+            </label>
+            <button>Registrar</button>
+        </form>
+    </main>
+</body>
+</html>
